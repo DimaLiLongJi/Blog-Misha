a0426a04f4bf3edfc1316483401baa694193761a
lilongji <woshililongji@163.com>
2018-03-10 21:52:33 +0800

    some change

--- js/手动增加页面缩放.js
+++ js/手动增加页面缩放.js
@@ -1,19 +1,29 @@
-componentWillMount() {
-  if (this.context.isMobile) {
-    this.metaContent = document.querySelector('meta[name=viewport]').content;
+class Index extends React.Component {
+  componentWillMount() {
+    if (this.context.isMobile) {
+      this.metaContent = document
+        .querySelector('meta[name=viewport]')
+        .content;
+    }
   }
-}
 
-componentDidMount() {
-  if (this.context.isMobile) {
-    const content = this.metaContent;
-    const newContent = content.replace(/maximum-scale=(\d*(\.\d*)?),/g, 'maximum-scale=10,').replace(/user-scalable=no/g, 'user-scalable=yes');
-    document.querySelector('meta[name=viewport]').content = newContent;
+  componentDidMount() {
+    if (this.context.isMobile) {
+      const content = this.metaContent;
+      const newContent = content
+        .replace(/maximum-scale=(\d*(\.\d*)?),/g, 'maximum-scale=10,')
+        .replace(/user-scalable=no/g, 'user-scalable=yes');
+      document
+        .querySelector('meta[name=viewport]')
+        .content = newContent;
+    }
   }
-}
 
-componentWillUnmount() {
-  if (this.context.isMobile) {
-    document.querySelector('meta[name=viewport]').content = this.metaContent;
+  componentWillUnmount() {
+    if (this.context.isMobile) {
+      document
+        .querySelector('meta[name=viewport]')
+        .content = this.metaContent;
+    }
   }
 }
