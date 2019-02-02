4add3e5615986aecabbdf1cb4280441a6514bb41
lilongji <woshililongji@163.com>
2019-02-02 12:02:43 +0800

    update cookie

--- js/myCookie.js
+++ js/myCookie.js
@@ -1,23 +1,32 @@
-// 保存cookie
-function setCookie(name,value,setdate){
-	var mydate=new Date(); //设置保存时间
-	mydate.setDate(mydate.getDate()+setdate);
-	document.cookie=name+'='+value+';expirse='+mydate;
-}
+const cookie = {
+  set: function (name, value, options) {
+    const cookieExpirse = options.expirse || 7;
+    const cookiePath = options.path || '/';
+    const cookieDomain = options.domain || '/';
+    let mydate = new Date();
+    mydate.setDate(mydate.getDate() + cookieExpirse);
+    if (cookieDomain) {
+      document.cookie = `${encodeURIComponent(name)}=${encodeURIComponent(JSON.stringify(value))};expirse=${mydate.toGMTString()};path=${cookiePath};domain=${cookieDomain}`;
+    } else {
+      document.cookie = `${encodeURIComponent(name)}=${encodeURIComponent(JSON.stringify(value))};expirse=${mydate.toGMTString()};path=${cookiePath}`;
+    }
+  },
+  get: function (name) {
+    if (!name) return null;
+    const cookieArray = document.cookie.split('; ');
+    for (let i = 0; i < cookieArray.length; i++) {
+      const cookieName = cookieArray[i].split('=');
+      if (cookieName[0] === encodeURIComponent(name)) {
+        const result = JSON.parse(decodeURIComponent(cookieName[1]));
+        if (result === '') return null;
+        return result;
+      }
+    }
+    return null;
+  },
+  remove: function (name) {
+    this.set(name, '', -1);
+  },
+};
 
-// 获取cookie的value
-function getCookie(name){
-	var cookieArray=document.cookie.split(';');//将cookie集分为无数数组
-	for(var i=0;i<cookieArray.length;i++){
-		var cookieName=cookieArray[i].split('=');
-		if(cookieName[0]==name){
-			return cookieName[1];
-		}
-	}
-	return '';
-}
-
-// 删除cookie
-function deletCookie(name){
-	setCookie(name,-1,-1);
-}
+module.exports = cookie;
